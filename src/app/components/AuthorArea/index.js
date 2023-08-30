6839442052f3f38ce84a4bdf5818cce3a06306df
JAMES CARVALLYO <jpcarvallyo@gmail.com>
2023-08-30 10:32:04 -0700

    clean up

--- src/app/components/AuthorArea/index.js
+++ src/app/components/AuthorArea/index.js
@@ -1,28 +1,10 @@
 import React from "react";
-import DOMPurify from "dompurify";
 import ContentEditable from "react-contenteditable";
-import { renderToString } from "react-dom/server";
 import "./style.css";
-import { ContentChangeEvent } from "../types";
-("../types");
-
-// interface AuthorAreaProps {
-//   handleHighlight: () => void;
-//   content: any;
-//   highlightedText: string[];
-//   onContentChange: (evt: ContentChangeEvent) => void;
-//   onContentBlur:
-// }
-
-const HTMLRenderer = ({ htmlContent }) => {
-  const sanitizedHtml = DOMPurify.sanitize(htmlContent);
-  return <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />;
-};
 
 const AuthorArea = ({
   handleHighlight,
   content,
-  highlightedText,
   onContentChange,
   onContentBlur,
 }) => {
